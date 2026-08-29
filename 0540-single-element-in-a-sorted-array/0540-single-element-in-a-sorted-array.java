e6360c1587a7ebc801f2e2dc995720e07b27d358
shivam kumar <mahurshivamkumar.@gmail.com>
2026-08-29 11:15:51 +0530

    Time: 0 ms (100%), Space: 52.9 MB (58.82%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,25 +1,34 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        // brute force solution--->
 
         int n = nums.length;
 
+        // For Eliminate the edge cases----->
         if (n == 1)
             return nums[0];
+        if (nums[0] != nums[1])
+            return nums[0];
+        if (nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
 
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                if (nums[i] != nums[i + 1]) {
-                    return nums[i];
-                }
-            } else if (i==n-1){
-                if(nums[n-1]!=nums[i-1]){
-                    return nums[i];
-                }
+        int lo = 1;
+        int hi = n - 2;
+
+        // look  odd even index 
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+                return nums[mid];
+            }
+
+            // for checking which part we eliminate 
+            if (mid % 2 == 1 && nums[mid - 1] == nums[mid] ||
+                    mid % 2 == 0 && nums[mid + 1] == nums[mid]) {
+                lo = mid + 1;
+
             } else {
-                if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]){
-                    return nums[i];
-                }
+                hi = mid - 1;
             }
         }
         return -1;
